05f15e3fa4bcc0faf5be11c7987422f5273ba6fc
davidmcdougal <@dmcdou8>
2024-08-09 16:54:00 -0600

    completed the work for the frontend of sign in and sign up

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -26,7 +26,10 @@ const RootLayout = () => {
   if (!fontsLoaded && !error) return null;
   return (
     <Stack>
-      <Stack.screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      {/* <Stack.Screen name="/search/[query]" options={{ headerShown: false }} /> */}
     </Stack>
   );
 };
